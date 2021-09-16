f6153809340198da510ebc68abcb95b77bf57047
vench juma <venchngogo22@gmail.com>
2021-09-16 16:27:27 +0300

    updated-to-do

--- list.js
+++ list.js
@@ -4,8 +4,6 @@ const form = document.getElementById('click');
 const input = form.querySelector('input');
 
 
-
-
 form.addEventListener('submit', (e) => {
     img.setAttribute("id", "hidden")
     e.preventDefault();
@@ -16,21 +14,31 @@ form.addEventListener('submit', (e) => {
    li.classList.add('list_')
    li.setAttribute('class', 'list_')
    ul.appendChild(li);
-   
+   const span =document.createElement('SPAN')
    const div = document.createElement('DIV');
    li.appendChild(div);
    div.setAttribute('class', 'checkbtn');
-
-   $('div').on('click', function(){
-    $('.list_').remove();
-    img.setAttribute("id", "image")
-
-    })
-
-   const span =document.createElement('SPAN')
    span.textContent = text
    li.appendChild(span)
+
+
+   div.addEventListener('click',(e)=>{
+       ul.removeChild(li)
+       img.setAttribute("id", "image")
+    })
+
+
+    li.addEventListener('click',(e)=>{
+       span.innerText = ''
+       const input = document.createElement('input')
+       input.type = 'text'
+       li.appendChild(input)
+       input.classList.add('list_2')
+    })
+
 });
+
+
 
 const icon = document.querySelector('#icon')
 
@@ -42,7 +50,6 @@ icon.addEventListener('click', () => {
     if(iswhite == true){
         container.classList.toggle('white')
         iswhite = false
-        
     }
     else {
         container.classList.add('white')
@@ -50,6 +57,8 @@ icon.addEventListener('click', () => {
     }
 
 })
+
+
 
 
 
